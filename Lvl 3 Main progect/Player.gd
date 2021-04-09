65168b3f42b782aa5f8c729494f5dbdfbc8e7550
ThomasLister14 <thomas@speights.net.nz>
2021-04-09 12:16:39 +1000

    Added Everything

---
+++ Lvl 3 Main progect/Player.gd
@@ -0,0 +1,84 @@
+extends KinematicBody2D
+
+#players variables 
+var velocity = Vector2(0,0)
+var SPEED = 300
+var SpeedBonus = 0
+var SprintYes = true
+var Jumps = 1
+var SpriteDireaction 
+var Dash = 600
+#const = constant (wont change/ fixed) 
+
+const JUMPFORCE = -600
+const GRAVITY = 30
+
+#signal timeout
+#const TIME_PERIOD = 5
+#func _physics_process(delta): does fucion at games refressh rate (60fps)
+func _physics_process(delta):
+	#Sprint Code
+	SpeedBonus = 0
+	if Input.is_action_just_pressed("run") and SprintYes == true:
+		SpeedBonus = 300
+		$Sprint.start()
+		print("start")
+		
+	
+	#Checks if "D" is pressed
+	if Input.is_action_pressed("right"):
+		#moves the player by the constant speed to the right 
+		velocity.x = SPEED +SpeedBonus +Dash
+		#$Sprite.play("Walk")
+		get_node( "Sprite" ).set_flip_h( false )
+		SpriteDireaction = false 
+		#print("move right")
+		
+		
+	#Checks id "A" is pressed 
+	elif Input.is_action_pressed("left"):
+		velocity.x = -SPEED - SpeedBonus - Dash
+		#$Sprite.play("Walk")
+		get_node( "Sprite" ).set_flip_h( true )
+		SpriteDireaction = true
+		#print("move left")
+	
+	
+	#dash Code 
+	if Input.is_action_just_pressed("Action_1") and SpriteDireaction == false:
+		velocity.x = SPEED + Dash
+	if Input.is_action_just_pressed("Action_1") and SpriteDireaction:
+		velocity.x = -SPEED - Dash 
+		
+	
+	#simulating Gravity with acceleration
+	velocity.y = velocity.y + GRAVITY
+	#print(velocity.y)
+	
+	#Player jump input
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMPFORCE
+		
+	if Input.is_action_just_pressed("jump") and is_on_floor() == false and Jumps == 1:
+		velocity.y = JUMPFORCE
+		Jumps = Jumps -1 
+	if is_on_floor():
+		Jumps = 1
+	
+	#means it wont constantly move down even on an object
+	#defines witch way is up so that the player can jump 
+	velocity = move_and_slide(velocity,Vector2.UP)
+	
+	#player slows down when key not pressed
+	velocity.x = lerp(velocity.x,0,0.1)
+	
+
+
+func _on_FallZone_body_entered(body):
+	get_tree().change_scene("res://TestLevelMovement.tscn")
+	print("restart")
+
+
+func _on_Timer_timeout():
+	print("time out")
+	SprintYes = false
