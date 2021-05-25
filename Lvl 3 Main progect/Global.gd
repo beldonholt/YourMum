16fb493a3bca52b312471a7a4a1ba27f17661d31
beldonholt <beldon.holt@gmail.com>
2021-05-26 09:53:44 +1200

    starting shadowman stuff

--- Lvl 3 Main progect/Global.gd
+++ Lvl 3 Main progect/Global.gd
@@ -1,7 +1,7 @@
 extends Node
 
 var ActiveScene = ""
-var PlayerPos = []
+var PlayerPos = [Vector2(-66.338,209.765)]
 var DifficultyTweak = 0.99
 var ShadowSpawnTimer = 100000
 var PlayerDeaths = 0
@@ -16,8 +16,10 @@ func loadSave():
 	print(PlayerDeaths)
 
 func AddPlayerPos(Pos):
-	if PlayerPos[-1] != Pos:
+	if (PlayerPos[-1] != Pos.round()):
 		if PlayerPos.size() < ShadowSpawnTimer:
-			PlayerPos.append()
-			print(Pos)
+			PlayerPos.append(Pos.round())
+			print(PlayerPos[-1])
 			print(PlayerPos.size())
+	pass
+
