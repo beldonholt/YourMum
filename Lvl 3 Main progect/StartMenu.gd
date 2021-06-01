736d1f962d05b34054e7f7731a04a3acbfff1f2d
ThomasLister14 <Thomas@speights.net.nz>
2021-06-02 09:29:29 +1200

    Added a title screen

--- Lvl 3 Main progect/StartMenu.gd
+++ Lvl 3 Main progect/StartMenu.gd
@@ -1,4 +1,9 @@
 extends Node
 func _ready():
 	$AnimationPlayer.play("TitleText")
+	$AnimationPlayer.play("BackGroundFade")
 	pass # Replace with function body.
+
+
+func _on_Button_pressed():
+	get_tree().change_scene("res://Levels/MainLevels/LevelOne.tscn")
