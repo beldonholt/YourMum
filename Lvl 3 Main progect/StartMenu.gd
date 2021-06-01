f300438c856eb33bd9c74b6de069e6a584071ad7
ThomasLister14 <Thomas@speights.net.nz>
2021-06-02 09:36:28 +1200

    Added space conforim for start screen

--- Lvl 3 Main progect/StartMenu.gd
+++ Lvl 3 Main progect/StartMenu.gd
@@ -1,8 +1,13 @@
 extends Node
+
+func _physics_process(_delta):
+	if Input.is_action_pressed("ui_accept"):
+		get_tree().change_scene("res://Levels/MainLevels/LevelOne.tscn")
+	
 func _ready():
 	$AnimationPlayer.play("TitleText")
 	$AnimationPlayer.play("BackGroundFade")
-	pass # Replace with function body.
+
 
 
 func _on_Button_pressed():
