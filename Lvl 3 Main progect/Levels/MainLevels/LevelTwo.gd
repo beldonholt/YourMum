bf083fe283fc9dc28c07962ee733cf8badb75057
bellyboi <beldon.holt@gmail.com>
2021-08-10 09:54:08 +1200

    yus, level 2

---
+++ Lvl 3 Main progect/Levels/MainLevels/LevelTwo.gd
@@ -0,0 +1,12 @@
+extends Node
+
+	#saving the scene
+func _ready():
+#	Global.SPEED = 300
+	#sets this scene as the current saved scen in global
+	if Global.ActiveScene == "Levels/MainLevels/LevelTwo.tscn":
+		$Player.position = Global.currentCPP
+	Global.sceneSave()
+	
+	Global.recordPos = true
+	
