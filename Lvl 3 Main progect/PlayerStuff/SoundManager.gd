a067becee0f17a31774336e34cf70edb90467149
Beldon Holt <beldon.holt@gmail.com>
2021-08-23 12:11:19 +1200

    trying to make sound better

--- Lvl 3 Main progect/PlayerStuff/SoundManager.gd
+++ Lvl 3 Main progect/PlayerStuff/SoundManager.gd
@@ -1,9 +1,15 @@
 extends Node
 
+var backgroundMusic = ["res://Assets/Audio/Another.mp3","res://Assets/Audio/Dungeon.mp3"]
+var musicNum = 0
+
 func _ready():
+	randomize()
+	musicNum = randi() % 2
+	print(backgroundMusic[musicNum])
+	$BackgroundMusic.stream = backgroundMusic[musicNum]
 	$BackgroundMusic.play()
 	pass
 
 func _process(delta):
-	if not $BackgroundMusic.is_playing():
-		$BackgroundMusic.play()
+	pass
