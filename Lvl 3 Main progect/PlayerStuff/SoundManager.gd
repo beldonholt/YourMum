537fc21120361ab4b333e6814da430583afc329c
beldonholt <beldon.holt@gmail.com>
2021-09-15 14:57:14 +1200

    push

--- Lvl 3 Main progect/PlayerStuff/SoundManager.gd
+++ Lvl 3 Main progect/PlayerStuff/SoundManager.gd
@@ -1,12 +1,20 @@
-extends Node
+extends AudioStreamPlayer
 
 var backgroundMusic = ["res://Assets/Audio/Another.mp3","res://Assets/Audio/Dungeon.mp3"]
 var musicNum = 0
 
 func _ready():
+	Music_Play()
+	pass
+	
+func _update():
+	if not playing:
+		Music_Play()
+
+func Music_Play():
 	randomize()
 	musicNum = randi() % 2
 	print(backgroundMusic[musicNum])
-	$BackgroundMusic.stream = load(backgroundMusic[musicNum])
-	$BackgroundMusic.play()
+	stream = load(backgroundMusic[musicNum])
+	play()
 	pass
