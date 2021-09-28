23aff90d5fe28a6e3e35a70c8ca96cb7c00db853
ThomasLister14 <Thomas@speights.net.nz>
2021-09-29 12:57:57 +1300

    someth

--- Lvl 3 Main progect/Levels/MainLevels/LevelTwo.gd
+++ Lvl 3 Main progect/Levels/MainLevels/LevelTwo.gd
@@ -4,9 +4,10 @@ extends Node
 func _ready():
 #	Global.SPEED = 300
 	#sets this scene as the current saved scen in global
-	Global.sceneSave()
+	
 	if Global.ActiveScene == "Levels/MainLevels/LevelTwo.tscn":
 		$Player.position = Global.currentCPP
+	Global.sceneSave()
 
 	
 	Global.recordPos = true
