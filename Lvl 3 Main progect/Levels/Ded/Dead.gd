3a67f376ad4c90f217222c8cb6478c3287d99ad0
beldonholt <beldon.holt@gmail.com>
2021-11-05 08:50:42 +1300

    in process of adding Win state

--- Lvl 3 Main progect/Levels/Ded/Dead.gd
+++ Lvl 3 Main progect/Levels/Ded/Dead.gd
@@ -1,7 +1,1 @@
-extends Node2D
 
-#when the button on the death scene is pressed loads scene 
-#from load scene cods
-func _physics_process(_delta):
-	if Input.is_action_pressed("ui_accept"):
-		$Button.loadscene()
